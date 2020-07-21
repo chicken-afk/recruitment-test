5ac026fd29866221fdded0d44e0ef77304be3f81
Yoke Endarto <yoke.endarto@yokesen.com>
2020-07-21 12:12:27 +0700

    new problem

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class ApiController extends Controller
+{
+    public function get_user_list(){
+
+      $request = $client->get(env('API_PATH').'/trade_list', [
+        'headers' => [
+          'X-Authorization-Token' => md5(env('API_KEY')),
+          'X-Authorization-Time'  => time()
+        ]
+      ]);
+
+      $res = json_decode($request->getBody());
+
+      $traders = $res->data;
+      return view('test.trader',compact('res','traders'));
+    }
+}
